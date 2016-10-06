57622febde17fef6003c06e77bc7e0450854506e
Arie Kurniawan <hubungi.aja@gmail.com>
2016-10-06 14:09:10 +0700

    Update login.php

--- login.php
+++ login.php
@@ -7,13 +7,13 @@
    $query->execute(array($username));
    $hasil = $query->fetch();
    if($query->rowCount() == 0) {
-     echo "<div align='center'>Username Belum Terdaftar! <a href='../index.php?#small-dialog1'>Back</a></div>";
+     echo "<div align='center'>Username Belum Terdaftar! <a href='index.php?#small-dialog1'>Back</a></div>";
    } else {
      if($pass <> $hasil['password']) {
-       echo "<div align='center'>Password salah! <a href='../index.php#small-dialog'>Back</a></div>";
+       echo "<div align='center'>Password salah! <a href='index.php#small-dialog'>Back</a></div>";
      } else {
        $_SESSION['username'] = $hasil['username'];
-       header('location:../user.php');
+       header('location:user.php');
      }
    }
 ?>
