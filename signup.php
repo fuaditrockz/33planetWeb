4a5d58ad9330317174fd1a6fc8222a14ad350517
Arie Kurniawan <hubungi.aja@gmail.com>
2016-10-06 14:10:42 +0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -6,15 +6,15 @@
    $query = $db->prepare("SELECT * FROM users WHERE username = ?");
    $query->execute(array($username));   
    if($query->rowCount() != 0) {
-     echo "<div align='center'>Username Sudah Terdaftar! <a href='../index.php?#small-dialog1'>Back</a></div>";
+     echo "<div align='center'>Username Sudah Terdaftar! <a href='index.php?#small-dialog1'>Back</a></div>";
    } else {
      if(!$username || !$pass || !$email) {
-       echo "<div align='center'>Masih ada data yang kosong! <a href='../index.php?#small-dialog1'>Back</a>";
+       echo "<div align='center'>Masih ada data yang kosong! <a href='index.php?#small-dialog1'>Back</a>";
      } else {       
        $sql = $db->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
        $simpan = $sql->execute(array($username, $pass, $email));
        if($simpan) {
-         echo "<div align='center'>Pendaftaran Sukses, Silahkan <a href='../index.php?#small-dialog'>Login</a></div>";
+         echo "<div align='center'>Pendaftaran Sukses, Silahkan <a href='index.php?#small-dialog'>Login</a></div>";
        } else {
          echo "<div align='center'>Proses Gagal!</div>";
        }
